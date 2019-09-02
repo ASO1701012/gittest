985845c22f91cf17752f45db13be0f8c7f23a185
Nakamura Kenya <pinpondream@yahoo.co.jp>
2019-09-02 12:03:52 +0900

    [update]test.index.php
    echo aaa を追加

--- test.index.php
+++ test.index.php
@@ -1,12 +1,5 @@
-<?php require '../header.php'; ?>
-<?php
-/**
- * Created by PhpStorm.
- * User: kenya
- * Date: 2019/09/02
- * Time: 11:45
- */
 
+<?php
+echo "aaa";
 ?>
 
-<?php require '../footer.php'; ?>
